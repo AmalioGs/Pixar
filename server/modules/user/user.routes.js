12d4310c1da55eec6e6983d25ce94b91c32bc154
david <davidcf18@gmail.com>
2025-01-07 14:08:34 +0100

    Cambios de rutas deshechos

--- server/modules/user/user.routes.js
+++ server/modules/user/user.routes.js
@@ -7,8 +7,6 @@ const router = express.Router();
 router.get('/', userController.getUser)
 router.post('/register', userController.register)
 router.post('/login', userController.login);
-router.get('/pelis', userController.pelis);
 router.get('/findUserById', verifyToken, userController.findUserById);
-router.post('/loginPrueba', userController.prueba);
 
 export default router;
